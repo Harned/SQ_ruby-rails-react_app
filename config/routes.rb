9be8489d3c44d18f3646574e64bd35265bd867ca
Dmitry <aleshkindima25@gmail.com>
2023-04-05 13:39:25 +0600

    Что-то делается, что-то собирается, что-то умерло

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'persons/profile'
   root 'homepage#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
